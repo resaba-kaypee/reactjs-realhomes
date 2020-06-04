5a0988cd66a38f81a7416d42d7bc1aed06def965
kaypee <resaba.kaypee@gmail.com>
2020-06-04 10:30:30 +0700

    added Signup component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import SignIn from './components/forms/SignIn';
+import SignUp from './components/forms/SignUp';
 
 const App = () => {
   return (
     <div className="w-screen h-screen bg-gray-300">
-      <Navbar />
+      <SignIn />
     </div>
   );
 };
