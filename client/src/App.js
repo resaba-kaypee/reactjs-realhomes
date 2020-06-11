0bf3dddfd8db20c17b4d479b8e853837c6c58ae0
kaypee <resaba.kaypee@gmail.com>
2020-06-11 23:30:48 +0700

    added PropertyCards section and updated the card component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 import DummyContent from './components/pages/DummyContent';
 import Navbar from './components/layout/Navbar';
 import Background from './components/layout/Background';
+import PropertyCards from './components/card/PropertyCards';
 
 const App = () => {
   return (
-    <div className="relative w-screen h-screen font-sans bg-gray-300 ">
+    <div className="relative w-screen h-screen font-sans bg-gray-100 ">
       <Navbar />
       <Background />
-      <DummyContent />
+      <PropertyCards />
     </div>
   );
 };
--- client/src/components/card/PropertyCard.js
+++ client/src/components/card/PropertyCard.js
@@ -26,101 +26,99 @@ const PropertyCard = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-full">
-      {/* the div from the top is only use from styling and to be remove once finish */}
-
-      <div className="w-11/12 overflow-hidden bg-gray-300 rounded-lg shadow-xl md: 1/2 lg:w-1/3">
-        <div className="relative border-b-2 border-gray-100 pb-4/5">
+    <div className="w-full overflow-hidden bg-gray-300 rounded-lg shadow-xl md:mx-2">
+      <div className="relative border-b-2 border-gray-100 pb-4/5">
+        <a href="!#">
           <img
             className="absolute object-cover object-center w-full h-full"
             src={require(`../../assets/img/${property.imageCover}`)}
             alt="property card"
           />
+        </a>
+      </div>
+
+      {/* AGENT */}
+      <div className="relative -mt-10 text-right">
+        <div className="flex items-baseline justify-between p-4">
+          <div className="w-16 h-16 mr-5 overflow-hidden border-2 border-gray-100 rounded-full">
+            <img
+              src={require('../../assets/img/agent.png')}
+              alt="agent avatar"
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <div>
+            <a href="!#" className="text-yellow-600">
+              Sam Smith
+            </a>
+            , Agent
+          </div>
+        </div>
+      </div>
+
+      {/* INFORMATION */}
+      <div className="px-6 pb-4">
+        {/* --TITLE-- */}
+        <h4 className="pt-5 text-2xl font-semibold leading-tight truncate border-t-2 border-gray-400">
+          {property.title}
+        </h4>
+        <div className="pb-2">
+          <div className="flex justify-between py-2">
+            <div className="flex items-center text-yellow-600">
+              <SvgIcon name="marker" className="w-6 h-6 fill-current" />
+              <p className="text-black truncate">{property.location.address}</p>
+            </div>
+
+            <div className="flex items-center text-yellow-600">
+              <SvgIcon name="map" className="w-6 h-6 fill-current" />
+              <p className="text-black">{property.location.state}</p>
+            </div>
+          </div>
         </div>
 
-        {/* AGENT */}
-        <div className="relative -mt-10 text-right">
-          <div className="flex items-baseline justify-between p-4">
-            <div className="w-16 h-16 mr-5 overflow-hidden border-2 border-gray-100 rounded-full">
-              <img
-                src={require('../../assets/img/agent.png')}
-                alt="agent avatar"
-                className="object-cover w-full h-full"
+        {/* --DESC-- */}
+        <div className="pt-3 border-t-2 border-gray-400">
+          <div className="flex flex-wrap">
+            <p className="w-1/2 font-semibold text-black">
+              <SvgIcon
+                name="bed"
+                className="inline w-6 h-6 mr-1 fill-current"
               />
-            </div>
-            <div>
-              <a href="!#" className="text-yellow-600">
-                Sam Smith
-              </a>
-              , Agent
-            </div>
+              {property.bedrooms}
+            </p>
+            <p className="w-1/2 font-semibold text-right">
+              <SvgIcon
+                name="bath"
+                className="inline w-6 h-6 mr-1 fill-current"
+              />
+              {property.bathrooms}
+            </p>
+            <p className="w-1/2 pt-2 font-semibold">
+              <SvgIcon
+                name="ruler"
+                className="inline w-6 h-6 mr-1 fill-current"
+              />
+              {property.lotSize}{' '}
+              <span className="text-sm font-normal">sqft.</span>
+            </p>
+            <p className="w-1/2 pt-2 font-semibold text-right">
+              <SvgIcon
+                name="garage"
+                className="inline w-6 h-6 mr-1 fill-current"
+              />
+              {property.garage}
+            </p>
           </div>
         </div>
-
-        {/* INFORMATION */}
-        <div className="px-6 pb-4">
-          {/* --TITLE-- */}
-          <h4 className="pt-5 text-2xl font-semibold leading-tight truncate border-t-2 border-gray-400">
-            {property.title}
-          </h4>
-          <div className="pb-2">
-            <div className="flex justify-between py-2">
-              <div className="flex items-center text-yellow-600">
-                <SvgIcon name="marker" className="w-6 h-6 fill-current" />
-                <p className="text-black">{property.location.address}</p>
-              </div>
-
-              <div className="flex items-center text-yellow-600">
-                <SvgIcon name="map" className="w-6 h-6 fill-current" />
-                <p className="text-black">{property.location.state}</p>
-              </div>
-            </div>
-          </div>
-
-          {/* --DESC-- */}
-          <div className="pt-3 border-t-2 border-gray-400">
-            <div className="flex flex-wrap">
-              <p className="w-1/2 font-semibold text-black">
-                <SvgIcon
-                  name="bed"
-                  className="inline w-6 h-6 mr-1 fill-current"
-                />
-                {property.bedrooms}
-              </p>
-              <p className="w-1/2 font-semibold text-right">
-                <SvgIcon
-                  name="bath"
-                  className="inline w-6 h-6 mr-1 fill-current"
-                />
-                {property.bathrooms}
-              </p>
-              <p className="w-1/2 pt-2 font-semibold">
-                <SvgIcon
-                  name="ruler"
-                  className="inline w-6 h-6 mr-1 fill-current"
-                />
-                {property.lotSize}{' '}
-                <span className="text-sm font-normal">sqft.</span>
-              </p>
-              <p className="w-1/2 pt-2 font-semibold text-right">
-                <SvgIcon
-                  name="garage"
-                  className="inline w-6 h-6 mr-1 fill-current"
-                />
-                {property.garage}
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* STATUS & PRICE*/}
-        <div className="flex">
-          <p className="w-2/5 py-4 text-lg font-semibold text-center text-gray-100 capitalize bg-blue-600">
-            {property.status}
-          </p>
-          <p className="w-3/5 py-4 text-lg font-semibold text-center text-gray-100 bg-yellow-600">
-            ${property.price}
-          </p>
-        </div>
+      </div>
+      {/* STATUS & PRICE*/}
+      <div className="flex">
+        <p className="w-2/5 py-4 text-lg font-semibold text-center text-gray-100 capitalize bg-blue-600">
+          {property.status}
+        </p>
+        <p className="w-3/5 py-4 text-lg font-semibold text-center text-gray-100 bg-yellow-600">
+          ${property.price}
+        </p>
       </div>
     </div>
   );
