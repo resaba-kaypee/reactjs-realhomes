eff61a73a0252b9dbd0023a50e87ac60707b8fdc
kaypee <resaba.kaypee@gmail.com>
2020-06-24 21:04:36 +0700

    created SeachOptions form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
-import DummyContent from './components/pages/DummyContent';
-import SingleProperty from './components/pages/SingleProperty';
-import Navbar from './components/layout/Navbar';
+// import DummyContent from './components/pages/DummyContent';
+// import SingleProperty from './components/pages/SingleProperty';
+// import Navbar from './components/layout/Navbar';
 // import Welcome from './components/sections/Welcome';
 // import PropertyCards from './components/sections/PropertyCards';
 // import WhyChooseUs from './components/sections/WhyChooseUs';
 // import CallToAction from './components/sections/CallToAction';
 // import News from './components/sections/News';
-import Footer from './components/sections/Footer';
+// import Footer from './components/sections/Footer';
+import SeachOptions from './components/forms/SearchOptions';
 
 const App = () => {
   return (
@@ -19,10 +20,11 @@ const App = () => {
       <CallToAction />
       <News />
       <Footer /> 
-    */}
       <Navbar />
       <SingleProperty />
       <Footer />
+    */}
+      <SeachOptions />
     </div>
   );
 };
