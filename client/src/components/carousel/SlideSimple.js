8dcbbb3e61171caa013e9884f86b4e68ec864443
kaypee <resaba.kaypee@gmail.com>
2020-06-29 22:29:27 +0700

    updated SlideSimple, created PropertyCardSimple

--- client/src/components/carousel/SlideSimple.js
+++ client/src/components/carousel/SlideSimple.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from '@brainhubeu/react-carousel';
+import PropertyCardSimple from '../card/PropertyCardSimple';
 
 const SlideSimple = () => {
   const properties = [
@@ -76,37 +77,7 @@ const SlideSimple = () => {
       }}
     >
       {properties.map((property) => (
-        <div key={property.propertyId} className="p-2 overflow-hidden">
-          <div className="relative">
-            <a href="!#">
-              <img
-                className="object-cover object-center rounded"
-                src={require(`../../assets/img/property/${property.imageCover}`)}
-                alt={property.propertyId}
-              />
-            </a>
-          </div>
-          <div className="relative px-2 -mt-10">
-            <p className="text-xl font-bold leading-10 text-white">
-              ${property.price}
-            </p>
-            <div className="flex flex-wrap text-sm">
-              <p className="w-1/2">
-                {property.bedrooms} <span className="font-semibold">Beds</span>
-              </p>
-              <p className="w-1/2">
-                {property.bathrooms}{' '}
-                <span className="font-semibold">Baths</span>
-              </p>
-              <p className="w-1/2">
-                {property.garage} <span className="font-semibold">Garage</span>
-              </p>
-              <p className="w-1/2">
-                {property.areaSize} <span className="font-semibold">sqft.</span>
-              </p>
-            </div>
-          </div>
-        </div>
+        <PropertyCardSimple propert={property} />
       ))}
     </Carousel>
   );
