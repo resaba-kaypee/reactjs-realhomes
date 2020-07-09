f5beafd78f81c6eed0a21fc36b295e088585d7e4
kaypee <resaba.kaypee@gmail.com>
2020-07-10 04:41:19 +0700

    updated router for searching properties by location

--- router/viewRoutes.js
+++ router/viewRoutes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 const {
   checkIfNew,
-  getAllProperty,
   getProperty,
+  getAllProperty,
+  getPropertiesByLocation,
   featuredProperties,
 } = require('../controller/viewController');
 
@@ -13,6 +14,8 @@ router.use(checkIfNew);
 
 router.route('/featured-properties').get(featuredProperties, getAllProperty);
 
-router.route('/property/:slug').get(getProperty); // avalable for everyone
+router.route('/property/:slug').get(getProperty); // avalaible for everyone
+
+router.route('/properties/search').get(getPropertiesByLocation); // avalaible for everyone
 
 module.exports = router;
