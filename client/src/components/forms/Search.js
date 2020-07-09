1261ed72efd12394bcdc051e142837ea40297368
kaypee <resaba.kaypee@gmail.com>
2020-07-10 04:44:25 +0700

    updated search using useLocation

--- client/src/components/forms/Search.js
+++ client/src/components/forms/Search.js
@@ -1,23 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Redirect, withRouter } from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  withRouter,
+  useLocation,
+  useHistory,
+} from 'react-router-dom';
 import PropertyContext from '../../context/property/propertyContext';
 import SvgIcon from '../svg/SvgIcon';
 import { useContext } from 'react';
 
-const Search = (props) => {
+const Search = () => {
+  const history = useHistory();
+  const location = useLocation();
+
   const propertyContext = useContext(PropertyContext);
-  const { getPropertiesByLocation, properties, loading } = propertyContext;
+  const { getPropertiesByLocation } = propertyContext;
 
-  const [location, setLocation] = useState('FL');
+  const [value, setValue] = useState('FL');
+  const [query, setQuery] = useState('');
 
-  const redirectTo = () => props.history.push({
-    pathname: '/search',
-    search: `?location=${location}`
-  });
+  location.search = `?location[state]=${value}`;
+
+  const redirectTo = () => {
+    history.push(`/search${query}`);
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    getPropertiesByLocation(location);
+    getPropertiesByLocation(query);
     redirectTo();
   };
 
@@ -28,12 +37,13 @@ const Search = (props) => {
           className="w-full px-4 py-3 leading-tight text-gray-700 border border-r-0 border-gray-500 rounded rounded-r-none appearance-none focus:outline-none focus:bg-white focus:border-purple-500"
           type="search"
           placeholder="Search state..."
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
         <button
           type="submit"
           className="px-4 py-3 text-gray-200 transition duration-500 ease-in-out bg-gray-800 border border-l-0 border-gray-800 rounded rounded-l-none hover:bg-yellow-600 hover:border-yellow-600"
+          onClick={() => setQuery(location.search)}
         >
           <SvgIcon name="search" className="w-5 h-5 fill-current" />
         </button>
@@ -42,20 +52,4 @@ const Search = (props) => {
   );
 };
 
-// <form className="relative z-10 flex w-full mt-5 justify-left">
-//   <div className="flex flex-col w-full lg:flex-row lg:justify-center">
-//     <input
-//       type="search"
-//       placeholder="Search by state..."
-//       defaultValue=""
-//       className="px-4 py-4 border border-indigo-400 rounded lg:py-2 lg:rounded-l lg:rounded-r-none lg:flex-1 focus:border-indigo-600 focus:outline-none focus:shadow-outline"
-//     />
-//     <button
-//       type="submit"
-//       className="py-5 mt-5 text-2xl text-white transition duration-150 bg-indigo-600 rounded lg:px-6 lg:py-2 lg:mt-0 lg:rounded-r lg:rounded-l-none hover:bg-yellow-600 focus:outline-none focus:shadow-outline"
-//     >
-//       Search
-//     </button>
-//   </div>
-// </form>
 export default withRouter(Search);
