37fd34f918b13e7466573ad3df8d5bbc31a0752f
kaypee <resaba.kaypee@gmail.com>
2020-07-15 22:34:11 +0700

    built query string from query object and updated UI

--- client/src/components/forms/FilterOptions.js
+++ client/src/components/forms/FilterOptions.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import SvgIcon from '../svg/SvgIcon';
 
 const FilterOptions = () => {
+  const location = useLocation();
+  console.log(location);
+
+  const [filterObj, setFilterObj] = useState({});
+
+  const onChange = (e) =>
+    setFilterObj({
+      ...filterObj,
+      [e.target.name]: e.target.value,
+    });
+
+  const {
+    city,
+    category,
+    type,
+    priceMin,
+    priceMax,
+    bedroomsMin,
+    bedroomsMax,
+    bathroomsMin,
+    bathroomsMax,
+  } = filterObj;
+
+  const queryStr = Object.keys(filterObj)
+    .filter((key) => filterObj[key] !== null && filterObj[key] !== '')
+    .map(
+      (key) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(filterObj[key])}`
+    )
+    .join('&');
+
+  const filterSearch = () => {
+    location.search = location.search + '&' + queryStr;
+    console.log(filterObj);
+    console.log(queryStr);
+    console.log(location.search);
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
   };
+
   return (
-    <form className="w-full" onSubmit={onsubmit}>
+    <form className="w-full" onSubmit={onSubmit}>
       <div className="flex flex-col w-full pt-6 pb-3 xl:flex-row">
         <div className="w-full md:flex md:justify-between">
           {/***** CITY *****/}
@@ -20,15 +60,14 @@ const FilterOptions = () => {
             <div className="relative">
               <select
                 id="city"
-                name="city"
+                name="location[city]"
+                value={city}
+                onChange={onChange}
                 className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
               >
                 <option value="">All City</option>
-                <option value="miami">London</option>
-                <option value="03">Dhaka</option>
-                <option value="04">Singapore</option>
-                <option value="05">Comilla</option>
-                <option value="06">Delhi</option>
+                <option value="miami">Miami</option>
+                <option value="orlando">Orlando</option>
               </select>
               <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
                 <SvgIcon name="chevron-down" className="w-5 h-5 fill-current" />
@@ -47,7 +86,9 @@ const FilterOptions = () => {
             <div className="relative">
               <select
                 id="category"
-                name="category"
+                name="type[category]"
+                value={category}
+                onChange={onChange}
                 className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
               >
                 <option value="">All</option>
@@ -71,10 +112,12 @@ const FilterOptions = () => {
             <div className="relative">
               <select
                 id="type"
-                name='type'
+                name="type[type]"
+                value={type}
+                onChange={onChange}
                 className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
               >
-                <option value="all-types">All</option>
+                <option value="">All</option>
                 <option value="single-family home">Single-family Home</option>
                 <option value="multi-family home">Multi-family Home</option>
                 <option value="villa">Villa</option>
@@ -102,15 +145,19 @@ const FilterOptions = () => {
             <div className="flex justify-between">
               <div className="relative w-2/5">
                 <select
-                  id="price"
+                  id="priceMin"
+                  name="price[gte]"
+                  value={priceMin}
+                  onChange={onChange}
                   className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
                 >
-                  <option value="01">Min</option>
-                  <option value="02">02</option>
-                  <option value="03">03</option>
-                  <option value="04">04</option>
-                  <option value="05">05</option>
-                  <option value="06">06</option>
+                  <option value="">Min</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
                 </select>
                 <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
                   <SvgIcon
@@ -122,15 +169,19 @@ const FilterOptions = () => {
               <div className="self-end w-1/5 text-center text-gray-700">to</div>
               <div className="relative w-2/5">
                 <select
-                  id="price"
+                  id="priceMax"
+                  name="price[lte]"
+                  value={priceMax}
+                  onChange={onChange}
                   className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
                 >
-                  <option value="01">Max</option>
-                  <option value="02">02</option>
-                  <option value="03">03</option>
-                  <option value="04">04</option>
-                  <option value="05">05</option>
-                  <option value="06">06</option>
+                  <option value="">Max</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
                 </select>
                 <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
                   <SvgIcon
@@ -147,56 +198,121 @@ const FilterOptions = () => {
             <div className="w-2/5 md:w-1/2">
               <label
                 className="block mb-1 text-xs font-bold tracking-wide text-gray-700 uppercase"
-                htmlFor="beds"
+                htmlFor="bedrooms"
               >
                 Bedrooms
               </label>
-              <div className="relative">
-                <select
-                  className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
-                  id="beds"
-                >
-                  <option value="01">01</option>
-                  <option value="02">02</option>
-                  <option value="03">03</option>
-                  <option value="04">04</option>
-                  <option value="05">05</option>
-                  <option value="06">06</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
-                  <SvgIcon
-                    name="chevron-down"
-                    className="w-5 h-5 fill-current"
-                  />
+              <div className="flex justify-between">
+                <div className="relative w-2/5">
+                  <select
+                    id="bedrooms"
+                    name="bedrooms[gte]"
+                    value={bedroomsMin}
+                    onChange={onChange}
+                    className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
+                  >
+                    <option value="">Min</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                  </select>
+                  <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                    <SvgIcon
+                      name="chevron-down"
+                      className="w-5 h-5 fill-current"
+                    />
+                  </div>
+                </div>
+                <div className="self-end w-1/5 text-center text-gray-700">
+                  to
+                </div>
+                <div className="relative w-2/5">
+                  <select
+                    id="bedrooms"
+                    name="bedrooms[lte]"
+                    value={bedroomsMax}
+                    onChange={onChange}
+                    className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
+                  >
+                    <option value="">Max</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                  </select>
+                  <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                    <SvgIcon
+                      name="chevron-down"
+                      className="w-5 h-5 fill-current"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+
             <div className="w-1/5 md:w-0 md:px-2"></div>
             {/***** BATHROOMS *****/}
             <div className="w-2/5 md:w-1/2">
               <label
                 className="block mb-1 text-xs font-bold tracking-wide text-gray-700 uppercase"
-                htmlFor="baths"
+                htmlFor="bathrooms"
               >
                 Bathrooms
               </label>
-              <div className="relative">
-                <select
-                  className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
-                  id="baths"
-                >
-                  <option value="01">01</option>
-                  <option value="02">02</option>
-                  <option value="03">03</option>
-                  <option value="04">04</option>
-                  <option value="05">05</option>
-                  <option value="06">06</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
-                  <SvgIcon
-                    name="chevron-down"
-                    className="w-5 h-5 fill-current"
-                  />
+              <div className="flex justify-between">
+                <div className="relative w-2/5">
+                  <select
+                    id="bathrooms"
+                    name="bathrooms[gte]"
+                    value={bathroomsMin}
+                    onChange={onChange}
+                    className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
+                  >
+                    <option value="">Min</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                  </select>
+                  <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                    <SvgIcon
+                      name="chevron-down"
+                      className="w-5 h-5 fill-current"
+                    />
+                  </div>
+                </div>
+                <div className="self-end w-1/5 text-center text-gray-700">
+                  to
+                </div>
+                <div className="relative w-2/5">
+                  <select
+                    id="bathrooms"
+                    name="bathrooms[lte]"
+                    value={bathroomsMax}
+                    onChange={onChange}
+                    className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
+                  >
+                    <option value="">Max</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                  </select>
+                  <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                    <SvgIcon
+                      name="chevron-down"
+                      className="w-5 h-5 fill-current"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
@@ -206,6 +322,7 @@ const FilterOptions = () => {
           <div className="flex items-end justify-center mb-3 md:flex-grow">
             <button
               type="submit"
+              onClick={() => filterSearch()}
               className="w-full max-w-lg py-3 font-semibold text-white align-middle transition duration-500 ease-in-out bg-gray-800 rounded-full hover:bg-yellow-600 hover:border-yellow-600"
             >
               <SvgIcon
