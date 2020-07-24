f6a24a1298f775692ceee053766b8275b2ead232
kaypee <resaba.kaypee@gmail.com>
2020-07-24 14:53:14 +0700

    able to sort data by options

--- client/src/components/forms/SortProperty.js
+++ client/src/components/forms/SortProperty.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import PropertyContext from '../../context/property/propertyContext';
 import SvgIcon from '../svg/SvgIcon';
 
 const SortProperty = () => {
+  const propertyContext = useContext(PropertyContext);
+  const { sortBy } = propertyContext;
+
+  const onChange = (e) => {
+    sortBy(e.target.value);
+  };
+
   return (
     <form action="">
       <div className="px-2">
@@ -13,14 +21,15 @@ const SortProperty = () => {
         </label>
         <div className="relative inline-block ml-4">
           <select
-            className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
             id="state"
+            onChange={onChange}
+            className="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-500 rounded appearance-none focus:outline-none focus:bg-white"
           >
-            <option value="">Newest Property</option>
-            <option value="">Lowest Price</option>
-            <option value="">Highest Price</option>
-            <option value="">Largest Area size</option>
-            <option value="">Largest Lot size</option>
+            <option value="new">Newest Property</option>
+            <option value="price_asc">Lowest Price</option>
+            <option value="price_desc">Highest Price</option>
+            <option value="area_desc">Largest Area size</option>
+            <option value="lot_desc">Largest Lot size</option>
           </select>
           <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
             <SvgIcon name="chevron-down" className="w-5 h-5 fill-current" />
