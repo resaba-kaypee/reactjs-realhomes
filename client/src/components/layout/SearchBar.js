8a50e2ff6d497ae8f5be324ec442cfc978f35bd8
kaypee <resaba.kaypee@gmail.com>
2020-07-25 22:01:48 +0700

    updated rounded corners

--- client/src/components/layout/SearchBar.js
+++ client/src/components/layout/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar = () => {
     <div className="absolute bottom-0 w-full mb-48 ml-auto mr-auto lg:mb-32">
       <div className="flex justify-center px-4 sm:px-8 lg:px-10">
         <div className="relative w-full max-w-6xl p-8">
-          <div className="absolute inset-0 bg-gray-900 rounded-lg opacity-75"></div>
+          <div className="absolute inset-0 bg-gray-900 rounded opacity-75"></div>
           <div className="relative">
             <h1 className="text-4xl font-semibold text-yellow-500">
               Let's Find Your Best Property
