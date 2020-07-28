79ed6e2042c2d611f12d2a4874defbe53381d7b2
kaypee <resaba.kaypee@gmail.com>
2020-07-29 02:01:07 +0700

    updated new layout design

--- client/src/components/card/PropertyCardSimple.js
+++ client/src/components/card/PropertyCardSimple.js
@@ -3,33 +3,39 @@ import { NavLink } from 'react-router-dom';
 
 const PropertyCardSimple = ({ property }) => {
   return (
-    <div className="p-2 overflow-hidden">
-      <div className="relative">
-        <NavLink to={`/property/${property.slug}`}>
-          <img
-            className="object-cover object-center rounded"
-            src={require(`../../assets/img/property/${property.imageCover}`)}
-            alt={property.propertyId}
-          />
-        </NavLink>
-      </div>
-      <div className="relative px-2 -mt-10">
-        <p className="text-xl font-bold leading-10 text-white">
-          ${property.price}
-        </p>
-        <div className="flex flex-wrap text-sm">
-          <p className="w-1/2">
-            {property.bedrooms} <span className="font-semibold">Beds</span>
+    <div className="p-2">
+      <div className="overflow-hidden border rounded shadow">
+        <div className="relative">
+          <NavLink to={`/property/${property.slug}`}>
+            <img
+              className="object-cover object-center h-56 md:h-40 xl:h-56 w-72"
+              src={require(`../../assets/img/property/${property.imageCover}`)}
+              alt={property.propertyId}
+            />
+          </NavLink>
+        </div>
+        <div className="relative px-2 -mt-10">
+          <p className="text-xl font-bold leading-10 text-white">
+            ${property.price}
           </p>
-          <p className="w-1/2">
-            {property.bathrooms} <span className="font-semibold">Baths</span>
-          </p>
-          <p className="w-1/2">
-            {property.garage} <span className="font-semibold">Garage</span>
-          </p>
-          <p className="w-1/2">
-            {property.areaSize} <span className="font-semibold">sqft.</span>
-          </p>
+          <div className="flex flex-wrap text-sm">
+            <p className="w-1/2">
+              {property.bedrooms} <span className="font-semibold">Beds</span>
+            </p>
+            <p className="w-1/2">
+              {property.bathrooms} <span className="font-semibold">Baths</span>
+            </p>
+            <p className="w-1/2">
+              {property.garage} <span className="font-semibold">Garage</span>
+            </p>
+            <p className="w-1/2">
+              {property.areaSize} <span className="font-semibold">sqft.</span>
+            </p>
+          </div>
+          <div className="w-full pb-2 text-sm font-bold truncate">
+            <span className="capitalize">{property.location.city}, </span>
+            <span>{property.location.state}</span>
+          </div>
         </div>
       </div>
     </div>
