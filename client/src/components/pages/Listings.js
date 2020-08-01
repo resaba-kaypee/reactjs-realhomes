dc2c043d277fe20b360dc1c21947ba6634ef92c1
kaypee <resaba.kaypee@gmail.com>
2020-08-01 19:57:24 +0700

    change the way of current state or city being view

--- client/src/components/pages/Listings.js
+++ client/src/components/pages/Listings.js
@@ -14,8 +14,8 @@ const Listings = () => {
   const propertyContext = useContext(PropertyContext);
   const {
     getPropertiesByLocation,
-    location_state,
-    location_city,
+    location_states,
+    location_cities,
     properties,
     loading,
   } = propertyContext;
@@ -71,16 +71,12 @@ const Listings = () => {
       <section>
         <div className="px-8 mt-6">
           <h2 className="text-xl font-bold leading-8 text-gray-800">
-            {location_state !== null && location_city !== null ? (
-              location_city.length > 1 ? (
-                <span>{location_state[0]}</span>
-              ) : (
-                <>
-                  <span className="capitalize">{location_city[0]}</span>{' '}
-                  <span>{location_state[0]}</span>{' '}
-                </>
-              )
-            ) : null}{' '}
+            {location_states !== null && location_states.length === 1 ? (
+              <span>{location_states[0]} </span>
+            ) : null}
+            {location_cities !== null && location_cities.length === 1 ? (
+              <span className="capitalize">{location_cities[0]} </span>
+            ) : null}
             Real Estate & Homes for Sale or Rent
           </h2>
           <div className="flex items-end justify-between mt-4">
