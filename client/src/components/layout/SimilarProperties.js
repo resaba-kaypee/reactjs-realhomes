736a031d469c0bbcf6839edba81b04b2ecb37e17
kaypee <resaba.kaypee@gmail.com>
2020-08-06 14:50:22 +0700

    created similar properties component

---
+++ client/src/components/layout/SimilarProperties.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import PropertyContext from "../../context/property/propertyContext";
+import SlideSimple from "../carousel/SlideSimple";
+
+const SimilarProperties = ({ type }) => {
+  const location = useLocation();
+  
+  location.search = `?type[type]=${type}`;
+
+  const propertyContext = useContext(PropertyContext);
+  const { properties, loading, getSimilarProperties } = propertyContext;
+
+  useEffect(() => {
+    if (type) {
+      getSimilarProperties();
+    }
+  }, [type]);
+
+  return (
+    <>
+      <h2 className="text-lg font-bold text-gray-800 underline sm:text-2xl">
+        Similar Properties
+      </h2>
+      <div className="mt-6">
+        {properties !== null && !loading ? (
+          <SlideSimple properties={properties} />
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default SimilarProperties;
