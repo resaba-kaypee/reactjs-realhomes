6433e0eddc4a4da874366ef567c621599efee27d
kaypee <resaba.kaypee@gmail.com>
2020-08-07 21:32:42 +0700

    updated UI layout

--- client/src/components/pages/SingleProperty.js
+++ client/src/components/pages/SingleProperty.js
@@ -9,6 +9,83 @@ import PropertyDetails from "../layout/PropertyDetails";
 import HouseDescription from "../layout/HouseDescription";
 import SimilarProperties from "../layout/SimilarProperties";
 
+const PropertyInfo = ({ property }) => (
+  <div className="flex justify-center mt-12 md:mt-0">
+    <div className="w-full px-12 md:px-0 md:mr-8">
+      <div className="">
+        {/* --TITLE-- */}
+        <h4 className="text-2xl font-semibold leading-tight text-center text-white sm:text-3xl">
+          {property.title}
+        </h4>
+        <div className="pt-4 pb-2">
+          <div className="flex justify-between">
+            <div className="flex items-center text-yellow-600">
+              <SvgIcon name="marker" className="w-6 h-6 mr-1 fill-current" />
+              <p className="text-white truncate sm:text-lg">
+                {property.location.address}
+              </p>
+            </div>
+
+            <div className="flex items-center text-yellow-600">
+              <SvgIcon name="map" className="w-6 h-6 mr-1 fill-current" />
+              <p className="text-white sm:text-lg">{property.location.state}</p>
+            </div>
+          </div>
+        </div>
+        {/* --INFO-- */}
+        <div className="px-2 pt-3 mt-4 border-t-2 border-gray-400">
+          <div className="flex flex-wrap">
+            <p className="flex justify-between w-1/2 p-3">
+              <span className="text-yellow-600">
+                <SvgIcon
+                  name="bed"
+                  className="inline w-6 h-6 mr-1 fill-current"
+                />
+              </span>
+              <span className="text-white sm:text-lg">
+                {property.bedrooms} Beds
+              </span>
+            </p>
+            <p className="flex justify-between w-1/2 p-3">
+              <span className="text-yellow-600">
+                <SvgIcon
+                  name="bath"
+                  className="inline w-6 h-6 mr-1 fill-current"
+                />
+              </span>
+              <span className="text-white sm:text-lg">
+                {property.bathrooms} Baths
+              </span>
+            </p>
+            <p className="flex justify-between w-1/2 p-3">
+              <span className="text-yellow-600">
+                <SvgIcon
+                  name="ruler"
+                  className="inline w-6 h-6 mr-1 fill-current"
+                />
+              </span>
+              <span className="text-white sm:text-lg">
+                {property.lotSize} sqft.
+              </span>
+            </p>
+            <p className="flex justify-between w-1/2 p-3">
+              <span className="text-yellow-600">
+                <SvgIcon
+                  name="garage"
+                  className="inline w-6 h-6 mr-1 fill-current"
+                />
+              </span>
+              <span className="text-white sm:text-lg">
+                {property.garage} Garage
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 const SingleProperty = () => {
   const { slug } = useParams();
 
@@ -37,127 +114,51 @@ const SingleProperty = () => {
       {property !== null && !loaded ? (
         <>
           {/* INFORATION */}
-          <div className="relative w-full h-full bg-gray-900">
-            <div className="flex justify-center">
-              <div className="w-full max-w-5xl sm:px-12">
-                <div className="px-4 pt-12 pb-24">
-                  {/* --TITLE-- */}
-                  <h4 className="pt-5 text-2xl font-semibold leading-tight text-center text-white sm:text-3xl">
-                    {property.title}
-                  </h4>
-                  <div className="pt-4 pb-2">
-                    <div className="flex justify-between">
-                      <div className="flex items-center text-yellow-600">
-                        <SvgIcon
-                          name="marker"
-                          className="w-6 h-6 mr-1 fill-current"
-                        />
-                        <p className="text-white truncate sm:text-lg">
-                          {property.location.address}
-                        </p>
-                      </div>
-
-                      <div className="flex items-center text-yellow-600">
-                        <SvgIcon
-                          name="map"
-                          className="w-6 h-6 mr-1 fill-current"
-                        />
-                        <p className="text-white sm:text-lg">
-                          {property.location.state}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  {/* --INFO-- */}
-                  <div className="px-2 pt-3 mt-4 border-t-2 border-gray-400">
-                    <div className="flex flex-wrap">
-                      <p className="flex justify-between w-1/2 p-3">
-                        <span className="text-yellow-600">
-                          <SvgIcon
-                            name="bed"
-                            className="inline w-6 h-6 mr-1 fill-current"
-                          />
-                        </span>
-                        <span className="text-white sm:text-lg">
-                          {property.bedrooms} Beds
-                        </span>
-                      </p>
-                      <p className="flex justify-between w-1/2 p-3">
-                        <span className="text-yellow-600">
-                          <SvgIcon
-                            name="bath"
-                            className="inline w-6 h-6 mr-1 fill-current"
-                          />
-                        </span>
-                        <span className="text-white sm:text-lg">
-                          {property.bathrooms} Baths
-                        </span>
-                      </p>
-                      <p className="flex justify-between w-1/2 p-3">
-                        <span className="text-yellow-600">
-                          <SvgIcon
-                            name="ruler"
-                            className="inline w-6 h-6 mr-1 fill-current"
-                          />
-                        </span>
-                        <span className="text-white sm:text-lg">
-                          {property.lotSize} sqft.
-                        </span>
-                      </p>
-                      <p className="flex justify-between w-1/2 p-3">
-                        <span className="text-yellow-600">
-                          <SvgIcon
-                            name="garage"
-                            className="inline w-6 h-6 mr-1 fill-current"
-                          />
-                        </span>
-                        <span className="text-white sm:text-lg">
-                          {property.garage} Garage
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
+          <div className="relative w-full h-full p-6 bg-gray-900 lg:p-0">
+            <div className="flex flex-col px-8 pt-16 md:flex-row">
+              <div className="self-center md:w-1/2">
+                <PropertyInfo property={property} />
+              </div>
+              <div className="self-center w-full md:w-1/2">
+                <Slide cover={property.imageCover} images={property.images} />
               </div>
             </div>
           </div>
 
-          <div className="relative w-full">
+          <div className="w-full mt-12">
             <div className="flex justify-center">
-              <div className="w-full max-w-5xl">
+              <div className="w-full">
                 <div className="px-4 -mt-12 sm:px-12">
-                  {/* IMAGE SLIDE */}
-                  <div className="">
-                    <Slide
-                      cover={property.imageCover}
-                      images={property.images}
-                    />
-                  </div>
-
-                  {/* VIRTUAL TOUR */}
-                  <div className="mt-12">
-                    <h2 className="text-lg font-bold text-gray-800 underline sm:text-2xl">
-                      Virtual Tour
-                    </h2>
-                    <div className="relative mt-6 overflow-hidden pt-9/16">
-                      <iframe
-                        title={property.title}
-                        className="absolute top-0 left-0 w-full h-full"
-                        src="https://www.youtube.com/embed/yl3i6z8vi8w"
-                        allowFullScreen></iframe>
+                  <div className="flex flex-col mt-12 md:flex-row">
+                    {/* VIRTUAL TOUR */}
+                    <div className="px-2 md:w-1/2">
+                      <h2 className="text-lg font-bold text-gray-800 underline sm:text-2xl">
+                        Virtual Tour
+                      </h2>
+                      <div className="relative mt-6 overflow-hidden pt-9/16">
+                        <iframe
+                          title={property.title}
+                          className="absolute top-0 left-0 w-full h-full"
+                          src="https://www.youtube.com/embed/yl3i6z8vi8w"
+                          allowFullScreen></iframe>
+                      </div>
                     </div>
-                  </div>
 
-                  {/* --MAP-- */}
-                  <div className="mt-12">
-                    <h2 className="text-lg font-bold text-gray-800 underline sm:text-2xl">
-                      Map
-                    </h2>
-                    <div className="mt-6">
-                      <Map
-                        longitude={property.location.coordinates[0]}
-                        latitude={property.location.coordinates[1]}
-                      />
+                    {/* --MAP-- */}
+                    <div className="px-2 md:w-1/2">
+                      <h2 className="text-lg font-bold text-gray-800 underline sm:text-2xl">
+                        Map
+                      </h2>
+                      <div className="mt-6 border">
+                        <div className="relative overflow-hidden pt-9/16">
+                          <div className="absolute top-0 left-0 w-full h-full">
+                            <Map
+                              longitude={property.location.coordinates[0]}
+                              latitude={property.location.coordinates[1]}
+                            />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
