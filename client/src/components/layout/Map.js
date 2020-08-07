a0e24f5043dec53fd63d0aef3e06d7ee6728a9ee
kaypee <resaba.kaypee@gmail.com>
2020-08-07 21:38:01 +0700

    updated height

--- client/src/components/layout/Map.js
+++ client/src/components/layout/Map.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import SvgIcon from '../svg/SvgIcon';
+import React, { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import SvgIcon from "../svg/SvgIcon";
 
 const Map = ({ longitude, latitude }) => {
   const [viewport, setViewport] = useState({
@@ -13,11 +13,10 @@ const Map = ({ longitude, latitude }) => {
     <ReactMapGL
       {...viewport}
       width="100%"
-      height="400px"
+      height="100%"
       onViewportChange={setViewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      mapStyle="mapbox://styles/lightning-27/ck8xzwdai3vgc1inqyav9cvei"
-    >
+      mapStyle="mapbox://styles/lightning-27/ck8xzwdai3vgc1inqyav9cvei">
       <Marker latitude={latitude} longitude={longitude}>
         <div className="text-yellow-600">
           <SvgIcon name="marker" className="w-12 h-12 fill-current" />
