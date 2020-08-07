36a67d5ad39b027254956289e6d426e534709d1b
kaypee <resaba.kaypee@gmail.com>
2020-08-07 21:37:41 +0700

    created its own card

--- client/src/components/layout/MapListing.js
+++ client/src/components/layout/MapListing.js
@@ -1,7 +1,49 @@
-import React, { useState, Fragment } from 'react';
-import ReactMapGL, { Marker, Popup } from 'react-map-gl';
-import SvgIcon from '../svg/SvgIcon';
-import PropertyCardSimple from '../card/PropertyCardSimple';
+import React, { useState, Fragment } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { NavLink } from "react-router-dom";
+import SvgIcon from "../svg/SvgIcon";
+
+const PropertyCardSimple = ({ property }) => {
+  return (
+    <div className="p-2">
+      <div className="overflow-hidden border rounded shadow">
+        <div className="relative">
+          <NavLink to={`/property/${property.slug}`}>
+            <img
+              className="object-cover object-center h-56 md:h-40 xl:h-56 w-72"
+              src={require(`../../assets/img/property/${property.imageCover}`)}
+              alt={property.propertyId}
+            />
+          </NavLink>
+        </div>
+        <div className="relative px-2 -mt-10">
+          <p className="text-xl font-bold leading-10 text-white">
+            ${property.price.toLocaleString()}
+          </p>
+          <div className="flex flex-wrap text-sm">
+            <p className="w-1/2">
+              {property.bedrooms} <span className="font-semibold">Beds</span>
+            </p>
+            <p className="w-1/2">
+              {property.bathrooms} <span className="font-semibold">Baths</span>
+            </p>
+            <p className="w-1/2">
+              {property.garage} <span className="font-semibold">Garage</span>
+            </p>
+            <p className="w-1/2">
+              {property.areaSize && property.areaSize.toLocaleString()} <span className="font-semibold">sqft.</span>
+            </p>
+          </div>
+          <p className="text-sm font-bold">{property.location.address}</p>
+          <div className="w-full pb-2 text-sm font-bold truncate">
+            <span className="capitalize">{property.location.city}, </span>
+            <span>{property.location.state}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const MapListing = ({ properties }) => {
   const [viewport, setViewport] = useState({
@@ -22,46 +64,37 @@ const MapListing = ({ properties }) => {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       mapStyle="mapbox://styles/lightning-27/ck8xzwdai3vgc1inqyav9cvei"
       scrollZoom={true}
-      className="relative rounded"
-    >
+      className="relative rounded">
       {properties.map((property) => (
-        <Fragment key={property.propertyId}>
+        <Fragment key={property._id}>
           <Marker
             latitude={property.location.coordinates[1]}
             longitude={property.location.coordinates[0]}
-            offsetLeft={-12}
-          >
+            offsetLeft={-12}>
             <div
               onMouseOver={() =>
                 setShowPopup({
-                  [property.propertyId]: true,
+                  [property._id]: true,
                 })
               }
               onMouseOut={() => {
                 setShowProperty(false);
                 setShowPopup({});
               }}
-              onClick={() => setShowProperty(!showProperty)}
-            >
+              onClick={() => setShowProperty(!showProperty)}>
               <SvgIcon name="circle" className="w-6 h-6 cursor-pointer" />
             </div>
           </Marker>
-          {showPopup[property.propertyId] ? (
+          {showPopup[property._id] ? (
             <Popup
               tipSize={5}
               anchor="bottom"
               closeButton={false}
               latitude={property.location.coordinates[1]}
               longitude={property.location.coordinates[0]}
-              className="absolute z-10"
-            >
+              className="absolute z-10">
               <div className="flex flex-col items-center">
-                <div className="w-48 h-48">
-                  <PropertyCardSimple property={property} />
-                </div>
-                <h1 className="text-sm font-semibold">
-                  {property.location.address}
-                </h1>
+                <PropertyCardSimple property={property} />
               </div>
             </Popup>
           ) : null}
