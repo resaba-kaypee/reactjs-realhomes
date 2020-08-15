b4540d1abdf3ed57fa769d7375b84506dde93e5c
kaypee <resaba.kaypee@gmail.com>
2020-08-16 00:45:55 +0700

    minor changes to circle icon

--- client/src/components/svg/SvgIcon.js
+++ client/src/components/svg/SvgIcon.js
@@ -217,16 +217,7 @@ const getPath = (name, props) => {
         </g>
       );
     case "circle":
-      return (
-        <circle
-          cx="50"
-          cy="50"
-          r="40"
-          stroke="white"
-          strokeWidth="12"
-          fill="red"
-        />
-      );
+      return <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="12" />;
     case "close":
       return (
         <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z" />
