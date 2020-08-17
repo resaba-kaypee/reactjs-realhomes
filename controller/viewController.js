d7caeb44d37d75f9308221973a3d3ba84faa798d
kaypee <resaba.kaypee@gmail.com>
2020-08-17 23:40:15 +0700

    added routes for saving ang getting users saved properties

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,15 +1,16 @@
-const Property = require('../models/Property');
+const Property = require("../models/Property");
+const SavedProperty = require("../models/SavedProperty");
 
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const { getAll } = require('./handlerFactory');
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const { getAll } = require("./handlerFactory");
 
 exports.checkIfNew = catchAsync(async (req, res, next) => {
   const properties = await Property.find({
     newTagDateExpires: {
       $lt: Date.now(),
     },
-  }).select('newTag');
+  }).select("newTag");
 
   // if newTagDateExpires set newTag to false
   if (properties)
@@ -24,7 +25,7 @@ exports.checkIfNew = catchAsync(async (req, res, next) => {
 
 exports.newest = (req, res, next) => {
   req.query = {
-    type: { type: 'single-family home' },
+    type: { type: "single-family home" },
     limit: 6,
   };
   next();
@@ -32,13 +33,13 @@ exports.newest = (req, res, next) => {
 
 exports.featuredProperties = (req, res, next) => {
   req.query.limit = 6;
-  req.query.sort = '-price';
+  req.query.sort = "-price";
   next();
 };
 
 exports.getAffordableProperties = (req, res, next) => {
   req.query = {
-    type: { type: 'single-family home' },
+    type: { type: "single-family home" },
     price: { lte: 300000 },
     limit: 6,
   };
@@ -47,16 +48,16 @@ exports.getAffordableProperties = (req, res, next) => {
 
 exports.getProperty = catchAsync(async (req, res, next) => {
   const property = await Property.findOne({ slug: req.params.slug }).populate({
-    path: 'user',
-    fields: 'name photo email phoneNumber',
+    path: "user",
+    fields: "name photo email phoneNumber",
   });
 
   if (!property) {
-    return next(new AppError('There is no property with that name', 404));
+    return next(new AppError("There is no property with that name", 404));
   }
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: property,
   });
 });
@@ -66,9 +67,9 @@ exports.getCitiesByCurrentLocation = catchAsync(async (req, res, next) => {
 
   // if searching from all available city in a state
   if (location !== undefined) {
-    if (Object.keys(location).length > 1 && location.city === 'all')
+    if (Object.keys(location).length > 1 && location.city === "all")
       delete location.city;
-    else if (Object.keys(location).length === 1 && location.city === 'all') {
+    else if (Object.keys(location).length === 1 && location.city === "all") {
       // if coming from buy page then search all city
       req.query = {};
     }
@@ -79,7 +80,7 @@ exports.getCitiesByCurrentLocation = catchAsync(async (req, res, next) => {
   // get all available city in state
   if (location && location.state) {
     query = {
-      'location.state': location.state,
+      "location.state": location.state,
     };
   }
 
@@ -93,11 +94,36 @@ exports.getCitiesByCurrentLocation = catchAsync(async (req, res, next) => {
 
 exports.getAllApartment = (req, res, next) => {
   req.query = {
-    type: { type: 'apartment' },
+    type: { type: "apartment" },
     limit: 6,
   };
   next();
 };
+
+exports.getMySavedProperties = catchAsync(async (req, res, next) => {
+  // find all saved properties that match user id
+  const savedProperties = await SavedProperty.find({ user: req.user.id });
+  // find all property by ids saved by user
+  const propertyIds = savedProperties.map((id) => id.property);
+  // get all property by ids saved by user
+  const properties = await Property.find({ _id: { $in: propertyIds } });
+
+  res.status(200).json({
+    status: "success",
+    results: properties.length,
+    data: properties,
+  });
+});
+
+exports.saveProperty = catchAsync(async (req, res, next) => {
+  const { user, property } = req.body;
+  const myProperty = await SavedProperty.create({ user, property });
+
+  res.status(200).json({
+    status: "success",
+    data: myProperty,
+  });
+});
 
 exports.getAllProperty = getAll(Property);
 
