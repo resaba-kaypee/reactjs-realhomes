c7b583e0f695ce720f24c9bc256b05fc5d429020
kaypee <resaba.kaypee@gmail.com>
2020-08-17 23:39:16 +0700

    added routes for saving ang getting users saved properties

--- router/viewRoutes.js
+++ router/viewRoutes.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+
+const { protect, isLoggedIn } = require("../controller/authController");
 
 const {
   checkIfNew,
@@ -12,22 +14,30 @@ const {
   newest,
   getAffordableProperties,
   featuredProperties,
-} = require('../controller/viewController');
+  saveProperty,
+  getMySavedProperties,
+} = require("../controller/viewController");
 
 router.use(checkIfNew);
 
-router.route('/newest').get(newest, getAllProperty);
+router.route("/newest").get(newest, getAllProperty);
 
-router.route('/featured-properties').get(featuredProperties, getAllProperty);
+router.route("/featured-properties").get(featuredProperties, getAllProperty);
 
-router.route('/affordable').get(getAffordableProperties, getAllProperty);
+router.route("/affordable").get(getAffordableProperties, getAllProperty);
 
-router.route('/property/:slug').get(getProperty); // avalaible for everyone
+router.route("/property/:slug").get(getProperty); // avalaible for everyone
 
-router.route('/rentals').get(getAllApartment, getAllProperty);
+router.route("/rentals").get(getAllApartment, getAllProperty);
 
 router
-  .route('/properties/search')
+  .route("/properties/search")
   .get(getCitiesByCurrentLocation, getPropertiesByLocation); // avalaible for everyone
+
+// for users
+
+// after logging in
+router.get("/my-saved-properties", protect, getMySavedProperties);
+router.post("/save-property", protect, saveProperty);
 
 module.exports = router;
