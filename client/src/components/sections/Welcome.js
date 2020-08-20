dd29546441db0687249a534640ad5f25095801a3
kaypee <resaba.kaypee@gmail.com>
2020-08-20 19:24:00 +0700

    updated text and background image

--- client/src/components/sections/Welcome.js
+++ client/src/components/sections/Welcome.js
@@ -1,25 +1,23 @@
 import React from "react";
 import Home from "../../assets/img/home1.jpg";
-import Listing from "../../assets/img/listings.jpg";
-import Searchbar from "../layout/SearchBar";
 import Search from "../forms/Search";
 
 const Welcome = () => {
   return (
     <section
-      className="relative w-full bg-no-repeat h-2/3"
+      className="relative w-full bg-center bg-no-repeat h-2/3"
       style={{
-        backgroundImage: `url(${Listing})`,
+        backgroundImage: `url(${Home})`,
       }}>
-      <div className="absolute bottom-0 w-full mb-32 ml-auto mr-auto lg:mb-24">
+      <div className="absolute z-10 w-full h-full bg-gray-900 opacity-25"></div>
+      <div className="absolute bottom-0 z-20 w-full mb-20 ml-auto mr-auto sm:mb-32 lg:mb-24">
         <div className="flex justify-center px-4 sm:px-8 lg:px-10">
           <div className="relative w-full max-w-6xl p-8">
-            <div className="absolute"></div>
             <div className="relative mb-4">
-              <h1 className="text-4xl font-semibold text-yellow-500">
-                Discover Your Perfect Home
+              <h1 className="text-4xl font-semibold text-white">
+                Let's find a home that is perfect for you.
               </h1>
-              <p className="mt-2 text-lg text-yellow-500">
+              <p className="mt-2 text-lg text-white">
                 With the most complete source of homes for sale & real estate
                 near you
               </p>
