cdfd82b24b98576c61c4a8a1e8ee735a325df930
kaypee <resaba.kaypee@gmail.com>
2020-08-20 19:27:06 +0700

    changed color scheme and overall look

--- client/src/components/nav/Menu.js
+++ client/src/components/nav/Menu.js
@@ -9,7 +9,7 @@ const Menu = () => {
         <button
           // BURGER MENU BUTTON
           type="button"
-          className="block text-white"
+          className="block text-green-500"
           onClick={() => setIsOpen(!isOpen)}>
           <svg
             className="w-10 h-10 fill-current focus:outline-none"
@@ -25,40 +25,40 @@ const Menu = () => {
       <div
         className={
           (isOpen ? "block " : "hidden ") +
-          "absolute md:static md:block top-0 left-0 w-full mt-20 md:mt-0 bg-gray-900"
+          "absolute md:static md:block top-0 left-0 w-full mt-20 md:mt-0"
         }>
         <div
           className={
-            "md:justify-left md:items-center h-full md:mt-0 px-4 text-md lg:text-lg md:flex md:p-0 mb-2 md:mb-0"
+            "md:justify-left md:items-center h-full md:mt-0 px-4 text-md lg:text-lg md:flex md:p-0 mb-2 md:mb-0 text-gray-700 bg-white shadow md:shadow-none"
           }>
           <NavLink
             to="/realstateforsale"
-            activeClassName="bg-gray-800"
-            className="block px-4 py-1 mt-1 text-gray-200 rounded hover:bg-gray-800 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:bg-gray-800">
+            activeClassName="bg-gray-200 md:border-t-2 border-b-2 border-green-500 text-gray-900"
+            className="block px-4 py-1 mt-1 border-b hover:border-t-2 hover:border-b-2 hover:border-green-500 leading-13 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:border-green-500 focus:border-b-2">
             Buy
           </NavLink>
           <NavLink
             to="/sell"
-            activeClassName="bg-gray-700"
-            className="block px-4 py-1 mt-1 text-gray-200 rounded hover:bg-gray-800 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:bg-gray-800">
+            activeClassName="bg-gray-200 md:border-t-2 border-b-2 border-green-500 text-gray-900"
+            className="block px-4 py-1 mt-1 border-b hover:border-t-2 hover:border-b-2 hover:border-green-500 leading-13 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:border-green-500 focus:border-b-2">
             Sell
           </NavLink>
           <NavLink
             to="/rentals"
-            activeClassName="bg-gray-700"
-            className="block px-4 py-1 mt-1 text-gray-200 rounded hover:bg-gray-800 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:bg-gray-800">
+            activeClassName="bg-gray-200 md:border-t-2 border-b-2 border-green-500 text-gray-900"
+            className="block px-4 py-1 mt-1 border-b hover:border-t-2 hover:border-b-2 hover:border-green-500 leading-13 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:border-green-500 focus:border-b-2">
             Rent
           </NavLink>
           <NavLink
             to="/myhome"
-            activeClassName="bg-gray-700"
-            className="block px-4 py-1 mt-1 text-gray-200 rounded hover:bg-gray-800 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:bg-gray-800">
+            activeClassName="bg-gray-200 md:border-t-2 border-b-2 border-green-500 text-gray-900"
+            className="block px-4 py-1 mt-1 border-b hover:border-t-2 hover:border-b-2 hover:border-green-500 leading-13 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:border-green-500 focus:border-b-2">
             My Home
           </NavLink>
           <NavLink
             to="/about"
-            activeClassName="bg-gray-700"
-            className="block px-4 py-1 mt-1 text-gray-200 rounded hover:bg-gray-800 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:bg-gray-800">
+            activeClassName="bg-gray-200 md:border-t-2 border-b-2 border-green-500 text-gray-900"
+            className="block px-4 py-1 mt-1 border-b hover:border-t-2 hover:border-b-2 hover:border-green-500 leading-13 md:px-2 md:mt-0 md-ml-2 focus:outline-none focus:border-green-500 focus:border-b-2">
             About
           </NavLink>
         </div>
