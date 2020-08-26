81693e28315770b6c5e5c47547696918fd70b97c
kaypee <resaba.kaypee@gmail.com>
2020-08-26 23:03:12 +0700

    able to register user

--- client/src/components/forms/SignUp.js
+++ client/src/components/forms/SignUp.js
@@ -1,10 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
 import FormContext from "../../context/form/formContext";
 import Modal from "../layout/Modal";
+import Alerts from "../layout/Alerts";
 
 const SignUp = ({ setIsOpen }) => {
+  const authContext = useContext(AuthContext);
+  const { registerUser, error, success } = authContext;
   const formContext = useContext(FormContext);
   const { closeForms, setShowSignIn } = formContext;
+
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const { name, email, password, passwordConfirm } = value;
+
+  const onChange = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (!error || !success) {
+      setIsSubmitting(false);
+    }
+  }, [error, success]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    registerUser(value);
+  };
+
   return (
     <Modal>
       <div className="flex items-center justify-center w-screen h-screen">
@@ -17,7 +49,12 @@ const SignUp = ({ setIsOpen }) => {
               setIsOpen(false);
               closeForms();
             }}></button>
-          <form className="relative px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
+          <form
+            onSubmit={onSubmit}
+            className="relative px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
+            <div className="w-full h-20">
+              <Alerts />
+            </div>
             <button
               type="button"
               tabIndex="-1"
@@ -30,31 +67,37 @@ const SignUp = ({ setIsOpen }) => {
                 <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z" />
               </svg>
             </button>
-            <div className="mb-4">
+            <div className="mb-6">
               <label
                 className="block mb-2 text-sm font-bold text-gray-700"
-                htmlFor="fullName">
+                htmlFor="name">
                 Full Name
               </label>
               <input
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                id="fullName"
-                type="fullName"
+                className="w-full px-4 py-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                name="name"
                 placeholder="Full Name"
+                onChange={onChange}
+                value={name}
                 required
               />
             </div>
-            <div className="mb-4">
+            <div className="mb-6">
               <label
                 className="block mb-2 text-sm font-bold text-gray-700"
                 htmlFor="email">
                 Email
               </label>
               <input
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                className="w-full px-4 py-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 id="email"
                 type="email"
+                name="email"
                 placeholder="Email"
+                onChange={onChange}
+                value={email}
                 required
               />
             </div>
@@ -65,10 +108,13 @@ const SignUp = ({ setIsOpen }) => {
                 Password
               </label>
               <input
-                className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                className="w-full px-4 py-3 leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password"
+                name="password"
                 placeholder="******************"
+                onChange={onChange}
+                value={password}
                 required
               />
             </div>
@@ -79,18 +125,25 @@ const SignUp = ({ setIsOpen }) => {
                 Corfirm Password
               </label>
               <input
-                className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                className="w-full px-4 py-3 leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 id="passwordConfirm"
                 type="passwordConfirm"
+                name="passwordConfirm"
                 placeholder="******************"
+                onChange={onChange}
+                value={passwordConfirm}
                 required
               />
             </div>
             <div className="flex flex-col items-center justify-center">
               <button
-                className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                type="submit">
-                Sign Up
+                disabled={isSubmitting}
+                type="submit"
+                className={
+                  (isSubmitting ? "opacity-50 cursor-wait" : "opacity-100") +
+                  " px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                }>
+                {isSubmitting ? "Submitting..." : "Sign Up"}
               </button>
               <div className="flex flex-col items-center mt-4">
                 <a
@@ -109,10 +162,10 @@ const SignUp = ({ setIsOpen }) => {
                 </p>
               </div>
             </div>
+            <p className="text-xs text-center text-gray-700">
+              &copy;2020 Acme Corp. All rights reserved.
+            </p>
           </form>
-          <p className="text-xs text-center text-gray-700">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
         </div>
       </div>
     </Modal>
