3e7740ddf172405f4b3879e15e913acab6542534
kaypee <resaba.kaypee@gmail.com>
2020-08-28 22:42:58 +0700

    added private route

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import AuthContext from "../../context/auth/authContext";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, role, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        // @make this better
+        !isAuthenticated && loading ? (
+          <Redirect to="/" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
