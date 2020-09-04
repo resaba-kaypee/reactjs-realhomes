85bc64da32bb050f20952d183da92c2d17c526a3
kaypee <resaba.kaypee@gmail.com>
2020-09-04 22:15:09 +0700

    able to save and delete from the users list

--- client/src/components/layout/SavePropertyButton.js
+++ client/src/components/layout/SavePropertyButton.js
@@ -8,21 +8,33 @@ const SavePropertyButton = ({ _id }) => {
   const { isAuthenticated, user } = authContext;
 
   const propertyContext = useContext(PropertyContext);
-  const { saveProperty, user_property_list } = propertyContext;
+  const {
+    saveProperty,
+    user_property_list,
+    deletePropertyFromList,
+  } = propertyContext;
 
   const [saved, setIsSaved] = useState(false);
+  const [propertyId, setPropertyId] = useState(null);
 
   useEffect(() => {
+    // loop tru users property list
+    // if a property(props) is in the list(user_property_list) change icon to full heart by toggling [saved] to true
+    // else just use outlined heart
     if (
       user_property_list !== null &&
       user_property_list.some(({ property }) => property._id === _id)
     ) {
       setIsSaved(true);
+      setPropertyId(
+        user_property_list.filter((list) => list.property._id === _id)
+      );
     } else {
       setIsSaved(false);
     }
   }, [user_property_list, _id]);
 
+  // handle saving property to user's list
   const handleSave = () => {
     if (isAuthenticated && user) {
       saveProperty({
@@ -32,9 +44,19 @@ const SavePropertyButton = ({ _id }) => {
     }
   };
 
+  const handleDelete = () => {
+    if (isAuthenticated && user) {
+      deletePropertyFromList(propertyId[0]._id);
+    }
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
-    handleSave();
+    if (!saved) {
+      handleSave();
+    } else {
+      handleDelete();
+    }
   };
 
   return (
