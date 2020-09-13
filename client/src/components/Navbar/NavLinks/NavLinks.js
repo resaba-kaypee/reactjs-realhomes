a8cbf2704d73fdf179613d56da3695954a595eac
kaypee <resaba.kaypee@gmail.com>
2020-09-13 10:08:29 +0700

    added dropdown links for about-us and app

--- client/src/components/Navbar/NavLinks/NavLinks.js
+++ client/src/components/Navbar/NavLinks/NavLinks.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import AccountMenu from "../AccountMenu";
+import SvgIcon from "../../SvgIcon";
 
 const NavLinks = ({ isOpen }) => {
+  const [isDropped, setIsDroppped] = useState(false);
   return (
     <div
       className={`${
@@ -37,12 +39,41 @@ const NavLinks = ({ isOpen }) => {
           My Home
         </NavLink>
 
-        <NavLink
-          activeClassName="border-b-2 border-blue-700 text-gray-900"
-          className="inline-block px-4 py-2 text-lg no-underline transition duration-500 ease-in-out shadow hover:border-blue-400 hover:border-b-2 md:shadow-none leading-13 hover:text-black lg:-ml-2"
-          to="/about">
-          About
-        </NavLink>
+        <button
+          className={`${
+            isDropped &&
+            "fixed inset-0 right-0 z-20 w-full h-full cursor-default"
+          }`}
+          onClick={() => {
+            setIsDroppped(false);
+          }}></button>
+        <button
+          className="inline-block px-4 py-2 text-lg text-left no-underline transition duration-500 ease-in-out shadow group md:shadow-none leading-13 hover:text-black lg:-ml-2"
+          onClick={() => setIsDroppped(!isDropped)}
+          type="button">
+          About{" "}
+          <SvgIcon
+            name="chevron-down"
+            className="inline-block w-5 h-5 fill-current"
+          />
+          <nav
+            className={`${
+              isDropped ? "transform scale-100" : "transform scale-0"
+            } absolute w-64 h-40 p-2 text-left text-blue-600 origin-top bg-white border rounded-sm group-hover:scale-100 min-w-32 z-30`}>
+            <NavLink
+              activeClassName="border-b-2 border-blue-700 text-gray-900"
+              className="block px-4 py-1 my-1 border-b-2 rounded-sm shadow hover:border-blue-400 hover:bg-gray-100 hover:text-gray-800"
+              to="/about-us">
+              About Us
+            </NavLink>
+            <NavLink
+              activeClassName="border-b-2 border-blue-700 text-gray-900"
+              className="block px-4 py-1 my-1 border-b-2 rounded-sm shadow hover:border-blue-400 hover:bg-gray-100 hover:text-gray-800"
+              to="/about-app">
+              About App
+            </NavLink>
+          </nav>
+        </button>
       </nav>
       <AccountMenu />
     </div>
