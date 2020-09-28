00b693341bba35d3cccc4ec7e5eaeb95fbf379be
kaypee <resaba.kaypee@gmail.com>
2020-09-28 22:31:04 +0700

    added state for forgot password component

--- client/src/context/form/FormState.js
+++ client/src/context/form/FormState.js
@@ -1,12 +1,18 @@
 import React, { useReducer } from "react";
 import FormContext from "./formContext";
 import formReducer from "./formReducer";
-import { SHOW_SIGNUP, SHOW_SIGNIN, CLOSE_FORMS } from "../types";
+import {
+  SHOW_FORGOT_PASSWORD,
+  SHOW_SIGNUP,
+  SHOW_SIGNIN,
+  CLOSE_FORMS,
+} from "../types";
 
 const FormState = (props) => {
   const initialState = {
     showSignUp: false,
     showSignIn: false,
+    showForgotPassword: false,
   };
 
   const [state, dispatch] = useReducer(formReducer, initialState);
@@ -17,6 +23,9 @@ const FormState = (props) => {
   const setShowSignIn = () => {
     dispatch({ type: SHOW_SIGNIN });
   };
+  const setForgotPassword = () => {
+    dispatch({ type: SHOW_FORGOT_PASSWORD });
+  };
   const closeForms = () => {
     dispatch({ type: CLOSE_FORMS });
   };
@@ -26,8 +35,10 @@ const FormState = (props) => {
       value={{
         showSignIn: state.showSignIn,
         showSignUp: state.showSignUp,
+        showForgotPassword: state.showForgotPassword,
         setShowSignIn,
         setShowSignUp,
+        setForgotPassword,
         closeForms,
       }}>
       {props.children}
--- client/src/context/form/formReducer.js
+++ client/src/context/form/formReducer.js
@@ -1,23 +1,38 @@
-import { SHOW_SIGNUP, SHOW_SIGNIN, CLOSE_FORMS } from "../types";
+import {
+  SHOW_SIGNUP,
+  SHOW_SIGNIN,
+  CLOSE_FORMS,
+  SHOW_FORGOT_PASSWORD,
+} from "../types";
 
 export default (state, action) => {
   switch (action.type) {
     case SHOW_SIGNUP:
       return {
         ...state,
+        showForgotPassword: false,
         showSignUp: true,
         showSignIn: false,
       };
     case SHOW_SIGNIN:
       return {
         ...state,
+        showForgotPassword: false,
         showSignIn: true,
+        showSignUp: false,
+      };
+    case SHOW_FORGOT_PASSWORD:
+      return {
+        ...state,
+        showForgotPassword: true,
+        showSignIn: false,
         showSignUp: false,
       };
     case CLOSE_FORMS:
       return {
         showSignIn: false,
         showSignUp: false,
+        showForgotPassword: false,
       };
     default:
       return state;
