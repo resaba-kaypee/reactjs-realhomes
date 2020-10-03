d5e401bca4452ada0d9075a95e940c4d6b1277e9
kaypee <resaba.kaypee@gmail.com>
2020-10-04 05:25:42 +0700

    fix pagination to show correct currentpages

--- client/src/components/Listings/Pagination/Pagination.js
+++ client/src/components/Listings/Pagination/Pagination.js
@@ -9,10 +9,9 @@ const Pagination = () => {
   const propertyContext = useContext(PropertyContext);
   const { loading, nextPage, prevPage, totalResults } = propertyContext;
 
-  const [start, setStart] = useState(1);
+  const [pageIndex, setPageIndex] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [currentPages, setCurrentPages] = useState([]);
   const itemsPerPage = 6;
 
   //needed to show how many results (Showing 1{from} to 6{to} of 20{totalResults} results)
@@ -28,7 +27,6 @@ const Pagination = () => {
 
   useEffect(() => {
     if (!isNaN(num)) {
-      setStart(num);
       setCurrentPage(num);
     }
   }, [num]);
@@ -36,16 +34,33 @@ const Pagination = () => {
   useEffect(() => {
     if (totalResults && !loading) {
       const total = Math.ceil(totalResults / itemsPerPage);
-      const pages = [];
       setTotalPages(total);
-
-      for (let i = 0; i < total; i++) {
-        pages[i] = i + 1;
-      }
-      setCurrentPages(pages.splice(start - 1, 5));
     }
     // eslint-disable-next-line
   }, [totalResults, loading]);
+
+  const pages = [];
+
+  if (totalPages > 0) {
+    for (let i = 0; i < totalPages; i++) pages[i] = i + 1;
+  }
+
+  const currentPages = pages.splice(pageIndex, 4);
+
+  useEffect(() => {
+    if (currentPages.length > 0) {
+      const currentPageIndex = currentPages.indexOf(currentPage);
+
+      if (currentPageIndex > 2) {
+        if (currentPages.pop() !== totalPages) setPageIndex(pageIndex + 1);
+      }
+
+      if (currentPageIndex < 1 && currentPages.shift() !== 1)
+        setPageIndex(pageIndex - 1);
+    }
+
+    // eslint-disable-next-line
+  }, [currentPage]);
 
   const onPaginate = (pageNumber) => {
     // if you are on page 2 and you want to move to another page
@@ -53,7 +68,6 @@ const Pagination = () => {
     // set the current page to selected value
     if (pageNumber !== currentPage) {
       setCurrentPage(pageNumber);
-      setStart(pageNumber);
     }
 
     // direct the page to selected value
@@ -64,7 +78,6 @@ const Pagination = () => {
   const onNextPage = () => {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
-      setStart(start + 1);
     }
     history.push(`?${nextSearch}`);
   };
@@ -73,7 +86,6 @@ const Pagination = () => {
   const onPrevPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
-      setStart(start - 1);
     }
     history.push(`?${prevSearch}`);
   };
