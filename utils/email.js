534df85b8eb155cea399a6bf223cad4404d3d699
kaypee <resaba.kaypee@gmail.com>
2020-11-01 19:24:45 +0700

    only use mailtrap

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,27 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const pug = require('pug');
-const htmlToText = require('html-to-text');
+const pug = require("pug");
+const htmlToText = require("html-to-text");
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.firstName = user.name.split(' ')[0];
+    this.firstName = user.name.split(" ")[0];
     this.url = url;
     this.from = `Kaypee Resaba <${process.env.EMAIL}>`;
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
-      return nodemailer.createTransport({
-        service: 'Mailgun',
-        auth: {
-          user: process.env.MAILGUN_USERNAME,
-          pass: process.env.MAILGUN_PASSWORD,
-        },
-      });
-    }
+    //   if (process.env.NODE_ENV === 'production') {
+    //     // Mailgun
+    //     return nodemailer.createTransport({
+    //       service: 'Mailgun',
+    //       auth: {
+    //         user: process.env.MAILGUN_USERNAME,
+    //         pass: process.env.MAILGUN_PASSWORD,
+    //       },
+    //     });
+    //   }
 
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
@@ -36,11 +36,14 @@ module.exports = class Email {
   async send(template, subject) {
     // Send the acctual email
     // 1.) Render html for the email base on pug template
-    const html = pug.renderFile(`${__dirname}/../public/emailTemplate/${template}.pug`, {
-      firstName: this.firstName,
-      url: this.url,
-      subject,
-    });
+    const html = pug.renderFile(
+      `${__dirname}/../public/emailTemplate/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
 
     // 2.) Define email options
     const mailOptions = {
@@ -56,13 +59,13 @@ module.exports = class Email {
   }
 
   async sendWelcome() {
-    await this.send('welcome', 'Welcome to the Realhomes famliy!');
+    await this.send("welcome", "Welcome to the Realhomes famliy!");
   }
 
   async sendPasswordReset() {
     await this.send(
-      'passwordReset',
-      'Your password reset token is valid for 10mins.'
+      "passwordReset",
+      "Your password reset token is valid for 10mins."
     );
   }
 };
